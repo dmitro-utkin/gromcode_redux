b147d655e8a22805d929b0534c0b241d57d3c574
ДМИТРО <sssddd1366628@gmail.com>
2024-08-21 09:51:26 +0300

    task + 1

--- les4_react-with-redux/task1(react-redux-counter-sunglasses)/src/components/counter.jsx
+++ les4_react-with-redux/task1(react-redux-counter-sunglasses)/src/components/counter.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
-import { increment, decrement, reset } from "../counter.actions.js";
+import * as counterActions from "../counter.actions.js";
 
-const Counter = ({ counter, incr, decr, res }) => {
+const Counter = ({ counter, increment, decrement, reset }) => {
   return (
     <div className="counter">
-      <button className="counter__button" onClick={decr}>
+      <button className="counter__button" onClick={decrement}>
         -
       </button>
-      <span className="counter__value" onClick={res}>
+      <span className="counter__value" onClick={reset}>
         {counter}
       </span>
-      <button className="counter__button" onClick={incr}>
+      <button className="counter__button" onClick={increment}>
         +
       </button>
     </div>
@@ -24,12 +24,10 @@ const mapState = (state) => {
   };
 };
 
-const mapDispatch = (dispatch) => {
-  return {
-    incr: () => dispatch(increment()),
-    decr: () => dispatch(decrement()),
-    res: () => dispatch(reset()),
-  };
+const mapDispatch = {
+  increment: counterActions.increment,
+  decrement: counterActions.decrement,
+  reset: counterActions.reset,
 };
 
 const connector = connect(mapState, mapDispatch); // HOC (High Order Component)
