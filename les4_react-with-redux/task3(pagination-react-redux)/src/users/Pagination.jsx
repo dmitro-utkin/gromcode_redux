034ad2811fca7a0b04d15604bd783003ae29296a
ДМИТРО <sssddd1366628@gmail.com>
2024-08-22 11:12:22 +0300

    +1

--- les4_react-with-redux/task3(pagination-react-redux)/src/users/Pagination.jsx
+++ les4_react-with-redux/task3(pagination-react-redux)/src/users/Pagination.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) => {
+const Pagination = ({
+  goPrev,
+  goNext,
+  currentPage,
+  totalItems,
+  itemsPerPage,
+}) => {
   // Создаем переменные на основе свойств компонента
   const isPrevPageAvailable = currentPage > 0;
   const isNextPageAvailable = (currentPage + 1) * itemsPerPage < totalItems;
 
   return (
     <div className="pagination">
-      <button 
-        className={`pagination-button ${!isPrevPageAvailable ? 'disabled' : ''}`} 
-        onClick={goPrev} 
+      <button
+        className={`btn ${!isPrevPageAvailable ? "disabled" : ""}`}
+        onClick={goPrev}
         disabled={!isPrevPageAvailable}
       >
-        {isPrevPageAvailable ? '<' : ''} Prev
+        {isPrevPageAvailable ? "<" : ""}
       </button>
-      <span className="pagination-page">{currentPage + 1}</span>
-      <button 
-        className={`pagination-button ${!isNextPageAvailable ? 'disabled' : ''}`} 
-        onClick={goNext} 
+      <span className="pagination__page">{currentPage + 1}</span>
+      <button
+        className={`btn ${!isNextPageAvailable ? "disabled" : ""}`}
+        onClick={goNext}
         disabled={!isNextPageAvailable}
       >
-        Next {isNextPageAvailable ? '>' : ''}
+        {isNextPageAvailable ? ">" : ""}
       </button>
     </div>
   );
