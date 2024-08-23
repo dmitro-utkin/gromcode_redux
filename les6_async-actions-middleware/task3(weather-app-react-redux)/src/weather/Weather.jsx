fb0c7abd06c67182b254f9686f00fb7cd9960ea5
ДМИТРО <sssddd1366628@gmail.com>
2024-08-23 17:26:50 +0300

    try another way

--- les6_async-actions-middleware/task3(weather-app-react-redux)/src/weather/Weather.jsx
+++ les6_async-actions-middleware/task3(weather-app-react-redux)/src/weather/Weather.jsx
@@ -1,28 +1,37 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { getWeatherData } from './weather.actions';
 
-const Weather = () => {
-  const dispatch = useDispatch();
-  const cities = useSelector((state) => state.weather.cities);
+class Weather extends Component {
+  componentDidMount() {
+    this.props.getWeatherData();
+  }
 
-  useEffect(() => {
-    dispatch(getWeatherData());
-  }, [dispatch]);
+  render() {
+    const { cities } = this.props;
 
-  return (
-    <main className="weather">
-      <h1 className="weather__title">Weather data</h1>
-      <ul className="cities-list">
-        {cities.map((city) => (
-          <li key={city.id} className="city">
-            <span className="city__name">{city.name}</span>
-            <span className="city__temperature">{city.temperature} F</span>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+    return (
+      <main className="weather">
+        <h1 className="weather__title">Weather data</h1>
+        <ul className="cities-list">
+          {cities.map((city) => (
+            <li key={city.id} className="city">
+              <span className="city__name">{city.name}</span>
+              <span className="city__temperature">{city.temperature} F</span>
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  cities: state.weather.cities,
+});
+
+const mapDispatchToProps = {
+  getWeatherData,
 };
 
-export default Weather;
+export default connect(mapStateToProps, mapDispatchToProps)(Weather);
