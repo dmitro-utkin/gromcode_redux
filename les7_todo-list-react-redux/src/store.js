e518e5d9218787e3f6212a2c20679bd9e9aa9e54
ДМИТРО <sssddd1366628@gmail.com>
2024-08-29 19:02:44 +0300

    change version in package.json

--- les7_todo-list-react-redux/src/store.js
+++ les7_todo-list-react-redux/src/store.js
@@ -1,5 +1,5 @@
 import { combineReducers, createStore, compose, applyMiddleware } from "redux";
-import { thunk }from "redux-thunk";
+import  thunk  from "redux-thunk";
 import tasksReducer from "./tasks/tasks.reducer";
 
 const reducer = combineReducers({
